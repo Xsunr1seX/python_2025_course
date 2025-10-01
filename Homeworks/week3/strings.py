5fe3b4471014c09be7af85f319fc334a8473a4c9
Xsunr1seX <nikitataganov19@gmail.com>
2025-10-01 18:10:16 +0300

    strings homework3 added

--- Homeworks/week3/strings.py
+++ Homeworks/week3/strings.py
@@ -5,5 +5,34 @@ t2 = "abc##aa#b#c"
 s3 = "a#####b"
 t3 = "b"
 
-s = "abcd"
-t = "abcd#"
+s = s2
+t = t2
+# output = равны или нет
+
+result1 = []
+result2 = []
+
+len1 = len(s)
+len2 = len(t)
+
+for i in range(len1):
+    if s[i] != '#':
+        result1.append(s[i])
+    else:
+        result1.pop()
+
+for i in range(len2):
+    if t[i] != '#':
+        result2.append(t[i])
+    else:
+        result2.pop()
+
+print(str(result1), str(result2))
+
+if result1 == result2:
+    print("YES")
+else:
+    print("NO")
+
+
+
